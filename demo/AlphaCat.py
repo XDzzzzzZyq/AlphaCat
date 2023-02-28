d978334b976c46dd72989c3473d94337ea9a8c22
XdzzzzzzzYQ <xiaodouzizyq@gmail.com>
2023-02-28 09:26:52 +0800

    2.28 Jupyterbook on VS code

--- demo/AlphaCat.py
+++ demo/AlphaCat.py
@@ -1,7 +1,13 @@
 import sys
 import os
 
-sys.path.append("/workspaces/AlphaCat/src/AlphaCat")
+def f():
+    print(__file__)
+
+if "workspaces" in __file__:
+    sys.path.append("/workspaces/AlphaCat/src/AlphaCat")                            # for Github Codespace
+else:
+    sys.path.append(os.path.dirname(os.path.dirname(__file__))+"/src/AlphaCat")     # for local debug
 
 import Game as Game
 import dumbAI as DAI
